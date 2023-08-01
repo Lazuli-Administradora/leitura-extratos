e838075d61e573198afd0d5a586b8c792cc6e1eb
Lucca <lucca.viccini@engenharia.ufjf.br>
2023-08-01 17:57:56 -0300

    featÇ toques finais

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" data-theme='mytheme' className='bg-base-200'>
-      <Head />
+      <Head>
+        <title>Somador - Lazuli</title>
+        <link rel="icon" href="/lazuli.ico" />
+      </Head>
       <body style={{width:'100%', height:'100%'}}>
         <Main />
         <NextScript />
